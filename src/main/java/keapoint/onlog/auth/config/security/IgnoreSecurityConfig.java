b969d02ca66b054b386276a069b286fa41cfa29c
SeungHyeon <namsh1125@naver.com>
2023-10-15 17:35:03 +0900

    [Refactor] IgnoreSecurityConfig 리팩토링

--- src/main/java/keapoint/onlog/auth/config/security/IgnoreSecurityConfig.java
+++ src/main/java/keapoint/onlog/auth/config/security/IgnoreSecurityConfig.java
@@ -2,38 +2,37 @@ package keapoint.onlog.auth.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-/**
- * JWT 필요 없는 API 시큐리티 체인
- * */
-@EnableWebSecurity(debug = true)
 @Configuration
+@EnableWebSecurity
 public class IgnoreSecurityConfig {
 
-    @Order(1)
     @Bean
     public SecurityFilterChain IgnoreFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰으로 인증하므로 stateless
-                .and()
-                .headers().frameOptions().disable()
-                .and()
-                .formLogin().disable()//Form Based Authentication 을 사용하지 않음
-                .httpBasic().disable()
-                .logout().disable()
-                .csrf().disable()// HTTP Basic Authentication 을 사용하지 않음
-                .cors().disable()
-                .authorizeHttpRequests() //Http Request를 인가하라
-                .requestMatchers("/auth/kakao/callback").permitAll()
-                .requestMatchers("/v3/api-docs/**").permitAll()
-                .requestMatchers("/swagger-ui/index.html").permitAll()
-                .anyRequest().permitAll();
+                .httpBasic(AbstractHttpConfigurer::disable) // rest api 이므로 기본설정 사용안함. 기본설정은 비인증시 로그인폼 화면으로 리다이렉트 된다.
+                .csrf(AbstractHttpConfigurer::disable) // rest api이므로 csrf 보안이 필요없으므로 disable처리.
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt token으로 인증 -> 세션은 필요없으므로 생성안함.
+                )
+                .cors(AbstractHttpConfigurer::disable) // CORS(Cross-Origin Resource Sharing) 설정 비활성화.
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable) // X-Frame-Options 비활성화 (IFrame 사용 가능하도록).
+                )
+                .formLogin(AbstractHttpConfigurer::disable) // formLogin 대신 Jwt를 사용하기 때문에 disable로 설정
+                .logout(AbstractHttpConfigurer::disable) // 로그아웃 기능 비활성화.
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("auth/kakao/login").permitAll() // "/auth/kakao/login" 경로에 대한 요청은 인증 없이 허용
+                                .requestMatchers("/v3/api-docs/**").permitAll() // Swagger는 인증 없이 허용
+                                .requestMatchers("/swagger-ui/**").permitAll() // Swagger는 인증 없이 허용
+                );
 
         return http.build();
     }
