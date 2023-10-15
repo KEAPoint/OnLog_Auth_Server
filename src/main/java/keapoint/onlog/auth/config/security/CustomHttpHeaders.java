6ed373e2554b14c93b7dbb1ed8f2bf59322ec875
SeungHyeon <namsh1125@naver.com>
2023-10-15 12:03:16 +0900

    [Del] 불필요한 파일 및 코드 제거

--- src/main/java/keapoint/onlog/auth/config/security/CustomHttpHeaders.java
+++
@@ -1,7 +0,0 @@
-package keapoint.onlog.auth.config.security;
-
-import org.springframework.http.HttpHeaders;
-
-public class CustomHttpHeaders extends HttpHeaders {
-    public static final String KAKAO_AUTH = "Kakao-auth-code";
-}
